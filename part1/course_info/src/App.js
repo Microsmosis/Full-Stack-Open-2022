8aa38e7af1529ffdfe156a5b83da1c0c81ef0b7b
Luke <pilot@Luke.local>
2022-06-27 16:08:19 +0300

    part 1 done until 1.6

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -1,7 +1,7 @@
-const Header = ({course}) => {
+const Header = (props) => {
 	return (
 		<div>
-			<h1>{course}</h1>
+			<h1>{props.course.name}</h1>
 		</div>
 	)
 }
@@ -9,7 +9,7 @@ const Header = ({course}) => {
 const Part = (props) => {
 	return (
 		<div>
-			<p>{props.part} {props.amount}</p>
+			<p>{props.name} {props.exercises}</p>
 		</div>
 	)
 }
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
 	return (
 		<div>
-			<Part part={props.part1} amount={props.ex1} />
-			<Part part={props.part2} amount={props.ex2} />
-			<Part part={props.part3} amount={props.ex3} />
+			<Part name={props.parts.parts[0].name} exercises={props.parts.parts[0].exercises} />
+			<Part name={props.parts.parts[1].name} exercises={props.parts.parts[1].exercises} />
+			<Part name={props.parts.parts[2].name} exercises={props.parts.parts[2].exercises} />
 		</div>
 	)
 }
@@ -27,26 +27,36 @@ const Content = (props) => {
 const Total = (props) => {
 	return (
 		<div>
-			<p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+			<p>Number of exercises {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises}</p>
 		</div>
 	)
 }
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
-  
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
+
 	return (
-	  <div>
-		<Header course={course} />
-		<Content part1={part1} ex1={exercises1} part2={part2} ex2={exercises2} part3={part3} ex3={exercises3}/>
-		<Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
-	  </div>
+		<div>
+			<Header course={course} />
+			<Content parts={course}/>
+			<Total parts={course}/>
+		</div>
 	)
 }
 
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+	const { fn, name } = props
+
+	return (
+		<>
+			<button onClick={fn}>{name}</button>
+		</>
+	)
+}
+
+const Header = ({header}) => {
+	return (
+		<div>
+			<h1>{header}</h1>
+		</div>
+	)
+}
+
+const StatisticLine = ({fb}) => {
+	const iteratedObject = fb.grades.map(element => <tr key={element.id}><td>{element.name}</td> <td>{element.value}</td></tr>)
+
+	return (
+		<>
+			{iteratedObject}
+		</>
+	)
+}
+
+const Statistics = ({fb}) => {
+	return (
+		<div>
+			<table>
+				<tbody>
+					{fb.grades[3].value ? <StatisticLine fb={fb}/> : <tr><td>No feedback given</td></tr>}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+const App = () => {
+
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	const incrementGood = () => setGood(good + 1)
+	const incrementNeutral = () => setNeutral(neutral + 1)
+	const incrementBad = () => setBad(bad + 1)
+
+	let all = good + neutral + bad
+
+	const feedbacks = {
+		grades: [
+			{
+				id: 1,
+				name: 'good',
+				value: good
+			},
+			{
+				id: 2,
+				name: 'neutral',
+				value: neutral
+			},
+			{
+				id: 3,
+				name: 'bad',
+				value: bad
+			},
+			{
+				id: 4,
+				name: 'all',
+				value: all
+			},
+			{
+				id: 5,
+				name: 'average',
+				value: (good - bad) / all
+			},
+			{
+				id: 6,
+				name: 'positive',
+				value: (good / all) * 100 + ' %'
+			}
+		]
+	}
+
+	return (
+		<div>
+			<Header header={'give feedback'}/>
+			<Button name={'good'} fn={incrementGood}/>
+			<Button name={'neutral'} fn={incrementNeutral}/>
+			<Button name={'bad'} fn={incrementBad}/>
+			<Header header={'statistics'}/>
+			<Statistics fb={feedbacks}/>
+		</div>
+	)
+}
+
+export default App
