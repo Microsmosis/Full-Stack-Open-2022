b264e7eee5e5314b952eddb20dd4154590bf3df4
Luke <pilot@Luke.local>
2022-07-01 17:05:11 +0300

    part2, 2.14 infinite-rendering issue fixed

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -26,25 +26,37 @@ const Languages = ({ langs }) => {
 	);
 };
 
-const Country = ({ fullData }) => {
+const Weather = ({ weather }) => {
+	const iconSource ='http://openweathermap.org/img/wn/' + weather.weather[0].icon + '@2x.png';
+	return (
+		<div>
+			<h1>Weather in {weather.name}</h1>
+			<p>temperature {weather.main.temp} Celcius</p>
+			<img src={iconSource} alt='icon'></img>
+			<p>wind {weather.wind.speed} m/s</p>
+		</div>
+	); 
+};
+
+const Country = ({ fullData, weather }) => {
 	return (
 		<div>
 			<h1>{fullData[0].name.common}</h1>
 			<p>capital {fullData[0].capital}</p>
 			<p>area {fullData[0].area}</p>
-			<b>languages:</b>
+			<h2>languages:</h2>
 			<Languages langs={fullData} />
-
 			<img alt={fullData[0].name.common} src={fullData[0].flags.png}></img>
+			<Weather weather={weather} />
 		</div>
 	);
 };
 
-const Countries = ({ allCountriesData, test }) => {
+const Countries = ({ allCountriesData, test, weather }) => {
 	if (allCountriesData.length === 1) {
 		return (
 			<div>
-				<Country fullData={allCountriesData} />
+				<Country fullData={allCountriesData} weather={weather}/>
 			</div>
 		);
 	}
@@ -63,13 +75,17 @@ const Countries = ({ allCountriesData, test }) => {
 const App = () => {
 	const [countries, setCountries] = useState([]);
 	const [showCountries, setShowCountries] = useState('');
+	const [weather, setWeather] = useState([]);
+	const [switchW, setSwitchW] = useState([0]);
 
 	useEffect(() => {
-		axios.get('https://restcountries.com/v3.1/all').then((response) => {
-			setCountries(response.data);
-		});
-	}, []);
+			axios.get('https://restcountries.com/v3.1/all').then((response) => {
+				setCountries(response.data);
+				setSwitchW(1)
+			});
+	}, [showCountries]);
 
+	
 	let searchValue = showCountries.toLowerCase();
 
 	const countriesToShow = countries.filter((country) =>
@@ -84,6 +100,32 @@ const App = () => {
 		setShowCountries(event);
 	};
 
+	let capitalCity = ''
+	if(switchW === 1)
+	{
+		capitalCity = countriesToShow[0].capital;
+	}
+
+	useEffect(() => {
+		if(switchW === 1){
+			axios
+				.get(
+					'https://api.openweathermap.org/data/2.5/weather?q=' +
+						capitalCity +
+						'&appid=' +
+						process.env.REACT_APP_API_KEY + '&units=metric'
+				)
+				.then((response) => {
+					setWeather(response.data);
+					setSwitchW(0);
+				})
+				.catch((e) => {
+					console.log(e)
+				});
+		}
+	}, [capitalCity, switchW]);
+
+	console.log(switchW);
 	const iteratedObject = countriesToShow.map((country) => (
 		<p key={country.name.common}>
 			{country.name.common}{' '}
@@ -96,7 +138,7 @@ const App = () => {
 	return (
 		<div>
 			<Filter value={showCountries} fn={handleShowCountry} />
-			<Countries allCountriesData={countriesToShow} test={iteratedObject} />
+			<Countries allCountriesData={countriesToShow} test={iteratedObject} weather={weather}/>
 		</div>
 	);
 };
