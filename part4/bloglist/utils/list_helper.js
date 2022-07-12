482f42359f364436883251127d5f6f212b8d64a9
Luke <pilot@Luke.local>
2022-07-12 13:42:35 +0300

    4.5, part4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,7 +3,6 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-
 	const reducer = (sum, item) => {
 		return sum + item.likes
 	}
@@ -11,7 +10,12 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(reducer, 0); 
 }
 
+const favoriteBlog = (blogs) => {
+	return  blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current, 0)
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };
